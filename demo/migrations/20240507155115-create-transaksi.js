5c461d3c06d2bd6c69d044f620d0b01811ecb415
Aldo Januansyah H <108147181AldoJh@users.noreply.github.com>
2024-05-07 23:00:37 +0700

    first commit

---
+++ demo/migrations/20240507155115-create-transaksi.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('transaksis', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      cart_id: {
+        type: Sequelize.INTEGER
+      },
+      jne_id: {
+        type: Sequelize.INTEGER
+      },
+      total_transaksi: {
+        type: Sequelize.INTEGER
+      },
+      point_transaksi: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('transaksis');
+  }
+};
---
+++ demo/models/cart.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class cart extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  cart.init({
+    user_id: DataTypes.INTEGER,
+    product_id: DataTypes.INTEGER,
+    jumlah: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'cart',
+  });
+  cart.associate = function(models) {
+    cart.belongsTo(models.user, {foreignKey: 'user_id', as: 'user'});
+  };
+  cart.associate = function(models) {
+    cart.hasMany(models.transaksi, {foreignKey: 'cart_id', as: 'transaksi'});
+  }
+};
